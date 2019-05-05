93e6ee714540ba2acb78756c4b9aab9ca8fbf136
Sue Liu <sue.liu@mudano.com>
2019-05-05 10:39:32 +0100

    Finished chapter 9

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 import logging
 from logging.handlers import SMTPHandler
@@ -15,6 +16,8 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+mail = Mail(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
