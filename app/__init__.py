1cc6fb61dedf7d9fbd4ae377f7b3416e5f73b3c9
Sue Liu <sue.liu@mudano.com>
2019-05-05 12:55:52 +0100

    Finished facelift chapter

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,26 @@
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
-import logging
-from logging.handlers import SMTPHandler
-from logging.handlers import RotatingFileHandler
+from config import Config
 
-import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-mail = Mail(app)
-
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
